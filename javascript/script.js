4ae008bb60a549bac9752d62bb0b6aeb807ed631
Jasminefardella <jasmine.fardella@hotmail.com>
2023-12-06 09:53:50 +0800

    Add link back to home page

--- javascript/script.js
+++ javascript/script.js
@@ -43,7 +43,9 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">Back to all cities<a/>
+        `;
 }
 
 updateTime();
